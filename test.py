93167ceb5924e4bd1b74a0b2e48658e860362381
IV1T3 <alexander.groddeck@uni-hamburg.de>
2021-10-01 02:44:23 +0200

    Added PDFiDMain() support from buffer

--- test.py
+++ test.py
@@ -6,9 +6,18 @@ def main():
     options = pdfid.get_fake_options()
     options.scan = True
     options.json = True
+
+    # Analyze PDF from filenames
     list_of_dict = pdfid.PDFiDMain(filenames, options)
     print(list_of_dict)
 
+    # Analyze PDF from buffer
+    file_buffers = []
+    for filename in filenames:
+        with open(filename, "rb") as f:
+            file_buffers.append(f.read())
+    list_of_dict = pdfid.PDFiDMain([], options, file_buffers)
+    print(list_of_dict)
 
 if __name__ == '__main__':
     main()
